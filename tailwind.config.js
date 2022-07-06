36dfaa190be81a33925aed73fd7047464d80853b
Hadi Hasanpur <hadihasanpur@gmail.com>
2022-07-06 11:28:15 +0430

    office commit1

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // mode: 'jit',
+  content: ["./index.html"],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      vazir: ["Vazir"],
+    },
+    extend: {
+      lineHeight: {
+        0: "0",
+      },
+      padding: {
+        "1/3": "33.333333%",
+        "2/3": "66.6666%",
+      },
+    },
+  },
+
+};
